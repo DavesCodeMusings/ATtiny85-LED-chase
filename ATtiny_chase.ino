fbce150b3c59fd6a76c4aaa981ffca91835e91c4
Dave Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2026-04-11 21:37:34 -0500

    Update ATtiny_chase.ino improved with sleep instead of delay()

--- ATtiny_chase.ino
+++ ATtiny_chase.ino
@@ -1,7 +1,7 @@
 /*
     ATtiny85 3-LED chase sequence for bicycle indicator: 0, 1, 2, blank, repeat.
     Released to public domain by David Horton, creator of this code.
-    See also: https://github.com/DavesCodeMusings/ATtiny85-LED-chase
+    See also: https://github.com/DavesCodeMusings/ATtiny85-LED-chase/blob/main/blink_watchdog.ino
 
     To use:
     1. Set direction of chase sequence with logic level on input.
@@ -14,8 +14,32 @@
 #define LED1 1        // physical pin 6
 #define LED2 2        // physical pin 7
 #define DIRECTION 3   // physical pin 2
-#define DELAY_mS 500
 
+
+#include <avr/sleep.h>
+
+
+ISR (WDT_vect) {       // Fires on watchdog timer interrupt.
+  WDTCR |= bit(WDIE);  // Reenable watchdog interrupt each time. 
+}
+
+/*
+    Configure watchdog timer interrupt and put device into deep sleep.
+    This saves battery power compared to using traditional delay()
+*/
+void wdtSleep(void) {
+  const byte prescale = bit(WDP2) | bit(WDP0); // This setting equates to a half-second delay.
+  cli();                                       // Block interrupts while setting things up.
+  MCUSR = 0;                                   // Clear MCU status register of any previous interrupts.
+  WDTCR = bit(WDCE) | bit(WDE);                // Preparation to allow disabling watchdog timer.
+  WDTCR = 0;                                   // Reset watchdog to known state (disabled.)
+  WDTCR = bit(WDIE) | prescale;                // Enable watchdog interrupts, but not reset, and set prescaler.
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);         // Power down mode gives maximum battery saving potential.
+  sleep_enable();
+  sei();
+  sleep_mode();                                // Go to sleep
+  sleep_disable();                             // Program resumes here on wake-up (after interrupt routine fires.)
+}
 
 void setup() {
   pinMode(LED0, OUTPUT);
@@ -30,27 +54,27 @@ void loop() {
   if (digitalRead(DIRECTION) == HIGH) {  // counts upward 0, 1, 2, blank
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
     PORTB = PORTB | 0b00000001;  // Turn on only 0
-    delay(DELAY_mS);
+    wdtSleep();
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
     PORTB = PORTB | 0b00000010;  // Turn on only 1
-    delay(DELAY_mS);
+    wdtSleep();
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
     PORTB = PORTB | 0b00000100;  // Turn on only 2
-    delay(DELAY_mS);
+    wdtSleep();
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
-    delay(DELAY_mS);
+    wdtSleep();
   }
   else {  // counts downward 2, 1, 0, blank
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
     PORTB = PORTB | 0b00000100;  // Turn on only 2
-    delay(DELAY_mS);
+    wdtSleep();
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
     PORTB = PORTB | 0b00000010;  // Turn on only 1
-    delay(DELAY_mS);
+    wdtSleep();
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
     PORTB = PORTB | 0b00000001;  // Turn on only 0
-    delay(DELAY_mS);
+    wdtSleep();
     PORTB = PORTB & 0b11111000;  // Turn off 0, 1, 2
-    delay(DELAY_mS);
+    wdtSleep();
   }
 }
